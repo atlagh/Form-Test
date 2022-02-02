0cee1b9cc58fe1db0e7bd3699d3166381c560a9c
amine <atlagh.amine@gmail.com>
2022-02-02 12:08:57 +0100

    Updates

---
+++ precache-manifest.f1a6d0df54d572e67fbda115232189a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1efbcfa1a823c1423333cc59ce30cf58",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "2abf6636dfc894de6a2e",
+    "url": "/my-app/static/css/main.fec29646.chunk.css"
+  },
+  {
+    "revision": "4f7751d83ae9d25f14bf",
+    "url": "/my-app/static/js/2.0f859213.chunk.js"
+  },
+  {
+    "revision": "19f6e6f69f0a0ef916bd",
+    "url": "/my-app/static/js/3.f143dd49.chunk.js"
+  },
+  {
+    "revision": "2abf6636dfc894de6a2e",
+    "url": "/my-app/static/js/main.e6fd42ec.chunk.js"
+  },
+  {
+    "revision": "7fda4dd5f195760ee164",
+    "url": "/my-app/static/js/runtime~main.63cfa316.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/my-app/static/media/logo.06e73328.svg"
+  }
+]);
